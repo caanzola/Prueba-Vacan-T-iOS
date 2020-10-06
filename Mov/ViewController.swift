c1a0d7abac02be3095beda35e6546acfafb41ce6
Camilo Anzola <ca.anzola@uniandes.edu.co>
2020-10-06 11:48:22 -0500

    Inicial commit

---
+++ Mov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mov
+//
+//  Created by Camilo on 9/8/19.
+//  Copyright © 2019 Camilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
